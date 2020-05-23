25cd4e66c8efe01fe9464b399119135f408c3309
naymello <naymellofilho@gmail.com>
2020-05-23 14:18:00 -0300

    Add components on page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,27 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Image from "../components/image"
-import SEO from "../components/seo"
+import SEO from "../components/SEO"
+import Navigation from '../components/Navigation'
+import MyTitle from '../components/MyTitle'
+import About from '../components/About'
+import Technologies from '../components/Technologies'
+import Resume from '../components/Resume'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+
+import GlobalStyle from '../styles/GlobalStyle'
 
 const IndexPage = () => (
-  <div></div>
+  <>
+    <GlobalStyle />
+    <Navigation />
+    <MyTitle />
+    <About />
+    <Technologies />
+    <Resume />
+    <Contact />
+    <Footer />
+  </>
 )
 
 export default IndexPage
