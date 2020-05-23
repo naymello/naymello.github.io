41fa2efaadafd34835ba9fd46d1017bdd8452a11
naymello <naymellofilho@gmail.com>
2020-05-23 14:17:49 -0300

    Add Technologies component

---
+++ src/components/Technologies.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { H2 } from '../styles/Headers'
+import List from '../styles/List'
+
+const Technologies = () => {
+  return (
+    <>
+      <H2>Tecnologias</H2>
+      <List>
+        <li>JavaScript</li>
+        <li>CSS/SCSS</li>
+        <li>ReactJS</li>
+        <li>Webpack</li>
+        <li>Styled Components</li>
+        <li>NodeJS</li>
+        <li>Express</li>
+      </List>
+    </>
+  )
+}
+
+export default Technologies
