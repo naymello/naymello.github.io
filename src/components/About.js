85431b555866ab7ff074ddc55f74bc11dca44497
naymello <naymellofilho@gmail.com>
2020-05-23 14:16:50 -0300

    Add About component

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { H2 } from '../styles/Headers'
+import Text from '../styles/Text'
+
+const About = () => {
+  return (
+    <>
+      <H2>Sobre mim</H2>
+      <Text>
+        Desde os 6 anos, quando descobri que o computador na verdade não era o monitor 
+        e sim a caixa grande embaixo,venho me interessando cada vez mais nessa máquina 
+        resolutora de problemas. Decidi, mais tarde, aprender um pouco sobre programação 
+        e gostei tanto que quis cursar técnico em informática para entrar nessa área de 
+        eterno aprendizado. Aqui estou.
+      </Text>
+    </>
+  )
+}
+
+export default About
