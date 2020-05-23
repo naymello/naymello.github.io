200ec19dc1f52760fa10ed73c015cbfd078d4322
naymello <naymellofilho@gmail.com>
2020-05-23 14:17:12 -0300

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Logo from './Logo'
+
+import { H4 } from '../styles/Headers'
+import StyledFooter from '../styles/StyledFooter'
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      {/* <img src={logo} style={{width: '40px', color: '#4B4B4B'}}/> */}
+      <Logo fill="#4B4B4B"/>
+      <H4 style={{color: '#4B4B4B', margin: '0 20px'}}>Desenvolvido por Nay Mello</H4>
+    </StyledFooter>
+  )
+}
+
+export default Footer
