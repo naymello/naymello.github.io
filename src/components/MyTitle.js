060dbf10ea03097bc1852e08cfcb3a02cdf4fe56
naymello <naymellofilho@gmail.com>
2020-05-23 14:17:21 -0300

    Add MyTitle component

---
+++ src/components/MyTitle.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { H1, H4 } from '../styles/Headers'
+
+const MyTitle = () => {
+  return (
+    <>
+      <H1>Nay Mello</H1>
+      <H4>Desenvolvedor web de Valinhos - SP</H4>
+    </>
+  )
+}
+
+export default MyTitle
