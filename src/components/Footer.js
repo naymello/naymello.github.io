054c7bc1641ded899e9123097f9076a952e86c50
naymello <naymellofilho@gmail.com>
2020-05-24 13:16:18 -0300

    Delete useless comment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,6 @@ import StyledFooter from '../styles/StyledFooter'
 const Footer = () => {
   return (
     <StyledFooter>
-      {/* <img src={logo} style={{width: '40px', color: '#4B4B4B'}}/> */}
       <Logo fill="#4B4B4B"/>
       <H4 style={{color: '#4B4B4B', margin: '0 20px'}}>Desenvolvido por Nay Mello</H4>
     </StyledFooter>
