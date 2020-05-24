bd0fcf0a67ee59442f7fae336edc7202fd73f69d
naymello <naymellofilho@gmail.com>
2020-05-24 13:07:41 -0300

    Refactoring

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,21 +8,24 @@ import emailIcon from '../assets/email.svg'
 import whatsappIcon from '../assets/whatsapp.svg'
 
 const Contact = () => {
+  const LinkStyle = {
+    fontWeight: 'bold',
+    textDecoration: 'none',
+    color: '#040404',
+    fontSize: '1.6rem'
+  }
+
   return (
     <>
       <H2>Contato</H2>
       <Text>Gostou do que viu? Entre em contato comigo!</Text>
       <div style={{margin: '20px 0'}}>
         <img src={emailIcon} alt="Email" style={{margin: '0 20px'}}/>
-        <Link style={{fontWeight: 'bold', textDecoration: 'none', color: '#040404'}}>
-          naymellofilho@gmail.com
-        </Link>
+        <Link style={LinkStyle} > naymellofilho@gmail.com </Link>
       </div>
       <div style={{margin: '20px 0'}}>
         <img src={whatsappIcon} alt="Whatsapp" style={{margin: '0 20px'}}/>
-        <Link style={{fontWeight: 'bold', textDecoration: 'none', color: '#040404'}}>
-          (019) 99509 8282
-        </Link>
+        <Link style={LinkStyle} > (19) 99509 8282 </Link>
       </div>
     </>
   )
