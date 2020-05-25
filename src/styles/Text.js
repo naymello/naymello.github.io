608393ed19f90e6a40161d2349a0abb3d28344af
naymello <naymellofilho@gmail.com>
2020-05-25 16:07:17 -0300

    Fix some card margin/padding problems

--- src/styles/Text.js
+++ src/styles/Text.js
@@ -1,15 +1,9 @@
 import styled from 'styled-components'
 
-import Card from './Card'
-
 const Text = styled.p`
   font-size: 1.6rem;
   margin: 0 20px;
   line-height: 135%;
-
-  ${Card} & {
-    margin: 20px 0;
-  }
 `
 
 export default Text
