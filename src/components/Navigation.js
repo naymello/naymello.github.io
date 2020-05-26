400656459b7c3fc132b00bd707f2f4e1104f67ea
naymello <naymellofilho@gmail.com>
2020-05-26 19:33:48 -0300

    Add links and state to Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Logo from './Logo'
 
 import Navbar from '../styles/Navbar'
 import Wrapper from '../styles/Wrapper'
+import StyledLink from '../styles/StyledLink'
 
 import hamburger from '../assets/hamburger.svg'
 
-const Navigation = () => {
-  return (
-    <Navbar>
-      <Wrapper>
-        <Logo fill="#000" />
-        <img src={hamburger} style={{marginLeft: 'auto'}} />
-      </Wrapper>
-    </Navbar>
-  )
+class Navigation extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      active: false
+    }
+  }
+
+  handleHamburger = () => {
+    this.setState(prevState => ({
+      active: !prevState.active
+    }))
+  }
+
+  render() {
+    return (
+      <Navbar active={this.state.active}>
+        <Wrapper>
+          <Logo fill="#000" />
+          <ul>
+            <li><StyledLink to="/#sobre">Sobre mim</StyledLink></li>
+            <li><StyledLink to="/#tecnologias">Tecnologias</StyledLink></li>
+            <li><StyledLink to="/#projetos">Meus projetos</StyledLink></li>
+            <li><StyledLink to="/#contato">Contato</StyledLink></li>
+          </ul>
+          <img src={hamburger} onClick={this.handleHamburger}/>
+        </Wrapper>
+      </Navbar>
+    )
+  }
 }
 
 export default Navigation
