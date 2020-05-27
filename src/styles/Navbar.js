dc11398c2c85b125845b7f36dc7f8d2815839b66
naymello <naymellofilho@gmail.com>
2020-05-26 22:17:25 -0300

    Add responsive behavior to navbar

--- src/styles/Navbar.js
+++ src/styles/Navbar.js
@@ -1,10 +1,46 @@
 import styled from 'styled-components'
 
 const Navbar = styled.nav`
-  display: flex;
-  width: 100%;
-  padding: 25px 0;
   background-color: #FFF;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: ${props => props.active ? 'column' : 'row'};
+  height: ${props => props.active ? '190px' : '7.5vh'};
+  transition-timing-function: ease-in-out;
+
+  ul {
+    margin-left: auto;
+    list-style-type: none;
+    display: ${props => props.active ? 'block' : 'none'};
+    position: ${props => props.active ? 'absolute' : 'static'};
+
+    /* Will only work when absolute position is set */
+    top: 70px;
+    left: 5px;
+
+    @media (min-width: 768px) {
+      display: block;
+      margin-top: 0;
+    }
+
+    li {
+      float: ${props => props.active ? 'none' : 'left'};
+      margin: ${props => props.active ? '5px 15px' : '0 15px'};
+    }
+  }
+
+  /* Hamburger menu */
+  img {
+    margin-left: auto;
+
+    @media (min-width: 768px) {
+      display: none;
+    }
+  }
 `
 
 export default Navbar
--- src/styles/Wrapper.js
+++ src/styles/Wrapper.js
@@ -6,11 +6,17 @@ import StyledFooter from './StyledFooter'
 const Wrapper = styled.div`
   width: 100%;
   max-width: 1440px;
-  margin: 0 auto;
+  margin: 10vh auto 0;
 
-  ${Navbar} &,
+  ${Navbar} & {
+    display: flex;
+    margin: 2.6vh auto 0;
+    padding: 0 20px;
+  }
+
   ${StyledFooter} & {
     display: flex;
+    margin: 0 auto;
     padding: 0 20px;
   }
 `
