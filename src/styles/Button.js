1b727c8e3591f2a046766f8be00a246979f44300
naymello <naymellofilho@gmail.com>
2020-07-07 23:59:15 -0300

    Change color values to hexadecimal

--- src/styles/Button.js
+++ src/styles/Button.js
@@ -7,8 +7,8 @@ const Button = styled.button`
   padding: 10px 20px;
   border-radius: 5px;
   border-color: #040404;
-  background-color: ${props => props.primary ? '#040404' : 'white'};
-  color: ${props => props.primary ? 'white' : '#040404'};
+  background-color: ${props => props.primary ? '#040404' : '#FFF'};
+  color: ${props => props.primary ? '#FFF' : '#040404'};
 
   :hover {
     transition: .3s;
