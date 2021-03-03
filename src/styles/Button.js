7e3221718f24bf109d1765c12155c12aa4d03cf2
naymello <naymellofilho@gmail.com>
2021-03-03 18:47:18 -0300

    Redesign buttons

--- src/styles/Button.js
+++ src/styles/Button.js
@@ -4,17 +4,13 @@ const Button = styled.button`
   cursor: pointer;
   font-weight: bold;
   margin: 20px 0 20px 20px;
-  padding: 10px 20px;
-  border-radius: 5px;
-  border-color: #040404;
-  background-color: ${props => props.primary ? '#040404' : '#FFF'};
-  color: ${props => props.primary ? '#FFF' : '#040404'};
-
-  :hover {
-    transition: .3s;
-    background-color: ${props => props.primary ? '#111' : '#EEE'};
-    border-color: #111;
-  }
+  padding: ${props => props.mainButton ? '20px 40px' : '15px 30px'};
+  font-size: ${props => props.mainButton ? '1.8rem' : '1.3rem'};
+  border-color: ${props => props.backgroundColor};
+  border-width: 2px;
+  border-style: solid;
+  background-color: ${props => props.primary ? props.backgroundColor : '#FFF'};
+  color: ${props => props.fontColor};
 `
 
 export default Button
