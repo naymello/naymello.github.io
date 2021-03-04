a8bca0dea5bd2a3957f950e52c3428094328a361
naymello <naymellofilho@gmail.com>
2021-03-04 16:57:40 -0300

    Change footer color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
   return (
     <StyledFooter>
       <Wrapper>
-        <Logo backgroundColor="#4B4B4B" logoColor="#040404" />
+        <Logo backgroundColor="#4B4B4B" logoColor="#000000" />
         <H4 style={{ color: '#4B4B4B', margin: '0 20px' }}>Desenvolvido por Nay Mello</H4>
       </Wrapper>
     </StyledFooter>
