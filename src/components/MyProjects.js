856c8a8a3b10f91ea69fabea90f9cc68bd89fa51
naymello <naymellofilho@gmail.com>
2021-03-05 14:42:03 -0300

    Refactor class based component to React Hooks

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
 import Card from '../styles/Card'
 import { StandardText, BoldText, H2, H3 } from '../styles/Typography'
@@ -36,63 +36,55 @@ const Project = (props) => {
   )
 }
 
-export default class MyProjects extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      projects: [
-        {
-          id: 0,
-          name: 'My Musical Picture',
-          about: 'O My Musical Picture é uma aplicação web que faz uma imagem baseada no histórico de músicas ouvidas pelo usuário no Spotify. Foi usado a API do Spotify para pegar os dados, além das bibliotecas FileSaver e DomToImage para gerar a imagem.',
-          technologies: 'ReactJS, Gatsby, NodeJS, Express, Spotify Web API, Styled Components',
-          website: 'https://my-musical-picture.netlify.app/',
-          github: 'https://github.com/naymello/my-musical-picture',
-          logo: mmpLogo,
-          cover: mmpCover,
-          primaryColor: '#2929B1',
-          secondaryColor: '#F9FF3E'
-        },
-        {
-          id: 1,
-          name: 'Chord Chart',
-          about: 'O Chord Chart é uma aplicação web que mostra acordes do piano de forma dinâmica, facilitando o aprendizado no instrumento. Para isso, foi usado a manipulação de SVG (Scalable Vector Graphics) com a biblioteca React SVG Manipulation Tools.',
-          technologies: 'ReactJS, Create React App (CRA), Styled Components',
-          website: 'https://chord-chart.netlify.app/',
-          github: 'https://github.com/naymello/chord-chart',
-          logo: ccLogo,
-          cover: ccCover,
-          primaryColor: '#001724',
-          secondaryColor: '#8BC9E5'
-        }
-      ]
+const MyProjects = (props) => {
+  const [projects, setProjects] = useState([
+    {
+      id: 0,
+      name: 'My Musical Picture',
+      about: 'O My Musical Picture é uma aplicação web que faz uma imagem baseada no histórico de músicas ouvidas pelo usuário no Spotify. Foi usado a API do Spotify para pegar os dados, além das bibliotecas FileSaver e DomToImage para gerar a imagem.',
+      technologies: 'ReactJS, Gatsby, NodeJS, Express, Spotify Web API, Styled Components',
+      website: 'https://my-musical-picture.netlify.app/',
+      github: 'https://github.com/naymello/my-musical-picture',
+      logo: mmpLogo,
+      cover: mmpCover,
+      primaryColor: '#2929B1',
+      secondaryColor: '#F9FF3E'
+    },
+    {
+      id: 1,
+      name: 'Chord Chart',
+      about: 'O Chord Chart é uma aplicação web que mostra acordes do piano de forma dinâmica, facilitando o aprendizado no instrumento. Para isso, foi usado a manipulação de SVG (Scalable Vector Graphics) com a biblioteca React SVG Manipulation Tools.',
+      technologies: 'ReactJS, Create React App (CRA), Styled Components',
+      website: 'https://chord-chart.netlify.app/',
+      github: 'https://github.com/naymello/chord-chart',
+      logo: ccLogo,
+      cover: ccCover,
+      primaryColor: '#001724',
+      secondaryColor: '#8BC9E5'
     }
-  }
+  ])
 
-  render() {
-    const { projects } = this.state
-
-    return (
-      <div ref={this.props.reference} style={{ opacity: 0, transform: 'translate(0, 60px)' }}>
-        <H2 color="#FF9FFB" id="projetos">Meus projetos</H2>
-        <ProjectsSection>
-          {projects.map((project) => (
-            <Project
-              key={project.id}
-              name={project.name}
-              about={project.about}
-              technologies={project.technologies}
-              website={project.website}
-              github={project.github}
-              logo={project.logo}
-              cover={project.cover}
-              primaryColor={project.primaryColor}
-              secondaryColor={project.secondaryColor}
-            />
-          ))}
-        </ProjectsSection>
-      </div>
-    )
-  }
+  return (
+    <div ref={props.reference} style={{ opacity: 0, transform: 'translate(0, 60px)' }}>
+      <H2 color="#FF9FFB" id="projetos">Meus projetos</H2>
+      <ProjectsSection>
+        {projects.map((project) => (
+          <Project
+            key={project.id}
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+            website={project.website}
+            github={project.github}
+            logo={project.logo}
+            cover={project.cover}
+            primaryColor={project.primaryColor}
+            secondaryColor={project.secondaryColor}
+          />
+        ))}
+      </ProjectsSection>
+    </div>
+  )
 }
+
+export default MyProjects
