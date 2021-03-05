d774a1af3619ae03bd006f0c8309a3d63d64d888
naymello <naymellofilho@gmail.com>
2021-03-05 14:44:38 -0300

    Decrease translate Y

--- src/components/MoreInfo.js
+++ src/components/MoreInfo.js
@@ -5,7 +5,7 @@ import Button from '../styles/Button'
 
 const MoreInfo = (props) => {
   return (
-    <div ref={props.reference} style={{ opacity: 0, transform: 'translate(0, 60px)' }}>
+    <div ref={props.reference} style={{ opacity: 0, transform: 'translate(0, 20px)' }}>
       <H2 color="#EDFF20" id="contato">Deseja saber mais?</H2>
       <Button primary fontColor="#FFF" backgroundColor="#000" onClick={() => { window.open('mailto:naymellodev@gmail.com') }}>Entrar em contato</Button>
       <Button fontColor="#000" backgroundColor="#000" onClick={() => { window.open('/curriculo.pdf') }}>Ver currículo</Button>
